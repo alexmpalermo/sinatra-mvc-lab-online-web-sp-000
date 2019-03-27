1e9719a7e1dc14242f7e544e09c1079d314fce05
Alexandra Palermo <alexmpalermo@gmail.com>
2019-03-27 20:21:03 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,24 +6,17 @@ class PigLatinizer
     @user_input = user_input
   end
   
-  def find_out(input)
-    if input.match(/bcdfghjklmnpqrstvwxyz/)
-      input = c 
-    elsif input
-  
   def piglatinize(user_input)
     array = user_input.split(" ")
     array.map do |word|
       word_array = word.split
-      word_array.find_out(word_array[0])
-      
       if word_array[0].match(/AEIOUaeiou/)
         word_array << ["way"]
       elsif word_array[0].match(/bcdfghjklmnpqrstvwxyz/) && word_array[1].match(/AEIOUaeiou/)
       new = word_array.rotate
       new << ["ay"]
       elsif word_array[0].match(/bcdfghjklmnpqrstvwxyz/) && word_array[1].match(/bcdfghjklmnpqrstvwxyz/)
-      
+      new = word_array.rotate(2)
       .rotate
       word_array << ["a","y"]
       new_word = word_array.join
