0c09f82b5ccc9653da9131cd8e99f6d697fe0c36
Alexandra Palermo <alexmpalermo@gmail.com>
2019-03-27 20:11:09 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -9,10 +9,13 @@ class PigLatinizer
   def piglatinize(user_input)
     array = user_input.split(" ")
     array.map do |word|
-      word_array = word.split("")
+      word_array = word.split
       if word_array[0].match(/AEIOUaeiou/)
         word_array << ["way"]
-      elsif word_array[0].match(/bcdfghjklmnpqrstvwxyz/) && word_array[1].match()
+      elsif word_array[0].match(/bcdfghjklmnpqrstvwxyz/) && word_array[1].match(/AEIOUaeiou/)
+      new = word_array.rotate
+      new << ["ay"]
+      elsif word_array[0]
       
       .rotate
       word_array << ["a","y"]
