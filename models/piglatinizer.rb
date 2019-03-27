db8f641d38d3e1f203e1f35572b6739bfbb8e462
Alexandra Palermo <alexmpalermo@gmail.com>
2019-03-27 21:19:25 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -10,11 +10,27 @@ class PigLatinizer
     array = user_input.split(" ")
     array.map do |word|
       word_array = word.split("")
+       if word_array[0].match(/AEIOUaeiou/)
+        word_array << ["way"]
+        word_array.join
+
+      else
+        vowels = []
+        word_array.each_with_index do |letter, index|
+          if letter.match(/aeiou/)
+            vowels << index
+          end 
+        end
+         new = word_array.rotate(vowels[0].to_i)
+         new << ["ay"]
+         new.join
+       end
+       end 
+       
       
       
       
-      
-    end 
+    
      end 
      
     end 
